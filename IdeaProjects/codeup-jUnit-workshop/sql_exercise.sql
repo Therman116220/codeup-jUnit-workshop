13795e7f71137ce5799be4d928944c91ad015a9f
Therman116220 <thermanhaynes@gmail.com>
2022-03-16 08:53:21 -0500

    update

--- IdeaProjects/codeup-jUnit-workshop/sql_exercise.sql
+++ IdeaProjects/codeup-jUnit-workshop/sql_exercise.sql
@@ -1,4 +1,10 @@
 SELECT database();      /*NULL if not in applicable part of SQL Database*/
 USE sirius_example_db;    /*How to access db*/
 SHOW CREATE DATABASE sirius_example_db;
-DROP DATABASE sirius_example_db;   /*Perm delete db*/
+DROP DATABASE sirius_example_db;
+SHOW GRANTS for 'testUser'@'localhost';    /*See priv granted to user*/
+SELECT * FROM mysql.user;
+SELECT * from SERVERS;
+GRANT SELECT ON *.* TO 'databaseadmin'@'localhost';
+GRANT SELECT ON *.* TO 'databaseadmin'@'localhost' WITH GRANT OPTIONS ;
+SHOW databases;
